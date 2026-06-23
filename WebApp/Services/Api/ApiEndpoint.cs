b47813d531d9f8fd3161103ccc4f27b9b5379ba4
Thet Wai Aung <126937793+thetwaiaung-dev@users.noreply.github.com>
2026-06-23 14:52:34 +0630

    feat: Sample Role CRUD

--- WebApp/Services/Api/ApiEndpoint.cs
+++ WebApp/Services/Api/ApiEndpoint.cs
@@ -5,7 +5,10 @@ public sealed class ApiEndpoint
     public static string Login { get; } = "/api/Auth/Login";
 
     public static string Refresh { get; } = "api/Auth/Refresh";
-    
+
     public static string Logout { get; } = "api/Auth/Logout";
 
+    // Role
+    public static string Role { get; } = "api/Role";
+    public static string RoleById(long id) => $"api/Role/{id}";
 }
